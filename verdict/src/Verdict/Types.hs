530455345deea6eb8d900426a2290ad4c12ca1da
Alp Mestanogullari <alpmestan@gmail.com>
2015-10-14 20:32:56 +0200

    wip: error tree

--- verdict/src/Verdict/Types.hs
+++ verdict/src/Verdict/Types.hs
@@ -8,6 +8,7 @@ data a :&& b = a :&& b
 infixr 3 :&&
 data a :|| b = a :|| b
 infixr 2 :||
+data Not a
 
 ------------------------------------------------------------------------------
 -- * Other Base Terms
@@ -16,4 +17,9 @@ infixr 2 :||
 data IsEven
 data IsNonZero -- etc
 
-type Errors = [String]
+newtype Fix f a = Fix { unFix :: f (Fix f a) }
+
+data ErrorTree e
+  = Leaf e
+  | Or (ErrorTree e) (ErrorTree e)
+  | And (ErrorTree e) (ErrorTree e)

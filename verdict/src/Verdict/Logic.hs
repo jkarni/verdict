734cd3fabd9e4c434cccae24d3c1ef8669b87605
Julian K. Arni <jkarni@gmail.com>
2015-10-15 09:01:54 +0200

    More complete implication

--- verdict/src/Verdict/Logic.hs
+++ verdict/src/Verdict/Logic.hs
@@ -5,13 +5,21 @@ import GHC.Exts (Constraint)
 import GHC.TypeLits
 import Verdict.Types
 
-type family Implies a b :: Constraint
 
 type family Or (a :: Constraint) (b :: Constraint) :: Constraint where
     Or () x = ()
     Or x () = ()
 
-type instance Implies (a :&& b) c = (a `Implies` c) `Or` (b `Implies` c)
+type family Implies a b :: Constraint where
+    Implies (a :&& b) c = (a `Implies` c) `Or` (b `Implies` c)
+    Implies a (b :|| c) = (a `Implies` b) `Or` (a `Implies` c)
+    Implies a (Not a) = ('True ~ 'False)
+    Implies (Not a) a = ('True ~ 'False)
+    Implies a (Not (Not a)) = ()
+    Implies a b = Implies' a b
 
-type instance Implies (Length a) (MaxLength b) = a <= b
-type instance Implies (Length a) (MinLength b) = b <= b
+type family Implies' a b :: Constraint
+type instance Implies' (Length a) (MaxLength b) = a <= b
+type instance Implies' (Length a) (MinLength b) = b <= b
+type instance Implies' (MaxLength a) (MaxLength b) = a <= b
+type instance Implies' (MinLength a) (MinLength b) = b <= a

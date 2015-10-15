366840ea9ffb77125377b7c81fc55f6eab79367d
Julian K. Arni <jkarni@gmail.com>
2015-10-15 22:48:26 +0200

    ValidJSONKey and Spec

--- verdict-json/src/Verdict/JSON/Class.hs
+++ verdict-json/src/Verdict/JSON/Class.hs
@@ -1,8 +1,11 @@
 module Verdict.JSON.Class where
 
 import qualified Data.Text as Text
+import qualified Data.Map as Map
 import Verdict.JSON.Types
 
 class JsonVerdict a where
     jsonVerdict :: proxy a -> JsonConstraint Text.Text
 
+class JsonSchema a where
+    jsonVerdict :: proxy a -> Map.Map JSONKey
--- verdict-json/src/Verdict/JSON/Types.hs
+++ verdict-json/src/Verdict/JSON/Types.hs
@@ -1,4 +1,10 @@
-module Verdict.JSON.Types where
+module Verdict.JSON.Types
+    ( JsonConstraint(..)
+    , JSONKey
+    , ValidJSONKey
+    ) where
+
+import qualified Data.Text as Text
 
 data JsonConstraint a
     = Minimum Int
@@ -8,3 +14,19 @@ data JsonConstraint a
     | OtherError a
     deriving (Eq, Show, Read, Functor)
 
+data ValidJSONKey
+
+instance HaskVerdict ValidJSONKey where
+    haskVerdict _ = check (\x -> Text.foldr go x /= Bad) "Should have escaped single quotes"
+     where
+       go _ Bad        = Bad
+       go '\\' _       = Escaped
+       go '\'' Escaped = Continue
+       go '\'' _       = Bad
+       go _ _          = Continue
+
+data C = Continue | Escaped | Bad
+
+type JSONKey = Validated Validated Text.Text
+newtype Spec = Spec { unspec :: Map.Map JSONKey (Either (JsonConstraint Text.Text) Spec) }
+  deriving (Eq, Show, Read)

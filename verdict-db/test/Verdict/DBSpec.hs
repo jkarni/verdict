069e8f773bbf18e0acc9138aed1e41df0921e4f7
Julian K. Arni <jkarni@gmail.com>
2015-10-29 13:24:21 +0100

    compiling

--- verdict-db/test/Verdict/DBSpec.hs
+++ verdict-db/test/Verdict/DBSpec.hs
@@ -11,7 +11,7 @@ spec = describe "Verdict.DB" $ do
 querySpec :: Spec
 querySpec = describe "query" $ do
 
-  let ptable = insert [1..100] (empty :: VerdictDB '[MultipleOf 2] Int)
+  let ptable = insert [1..100] (empty :: VerdictDB '[Length] Int)
   it "returns only data that matches" $ do
 
     let r :: [Person LongName Adult]
